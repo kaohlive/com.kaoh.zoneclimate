ba5552bc9ccf0487a3ea49135d1deb4ac160749c
kaohlive <kaoh@kaoh.nl>
2024-12-22 23:34:25 +0100

    Added support for nested zones (childs)

--- widgets/zone-climate/api.js
+++ widgets/zone-climate/api.js
@@ -17,21 +17,41 @@ function createmeasurements(measurementid, devices)
   } 
 }
 
+// Collect all nested zones 
+function collectZones(zones, zoneId, traverse) { 
+  let collectedZones = [zoneId];
+  if(!traverse)
+    return collectedZones;
+  //Only do recursive calls if the user want to use nested zones
+  let subZones = zones.filter(zone => zone.parent === zoneId); 
+  for (let subZone of subZones) 
+  { 
+    collectedZones = collectedZones.concat(collectZones(zones, subZone.id, traverse)); 
+  } 
+  return collectedZones; 
+}
+
 module.exports = {
 
   async getZoneClimate({ homey, query }) {
     const filterCapabilities = ['measure_temperature', 'measure_humidity', 'measure_co2', 'measure_pm25', 'measure_etoh', 'measure_iaq', 'measure_tvoc', 'measure_co'];
-    const { zoneid } = query;
+    const { zoneid, includeChild } = query;
 
     homey.app.log('Init Homey API');
 
     this.homeyAPI = HomeyAPI.forCurrentHomey(homey); // Refresh
     this.homeyAPI.then(x => this.homeyAPI = x);
     await this.homeyAPI;
-    homey.app.log('Now retrieve the devices for this zone: '+zoneid);
+    homey.app.log('Now retrieve the devices for this homey');
     let myDevices = Object.values(await this.homeyAPI.devices.getDevices());
-    homey.app.log('Filter devices for zone='+zoneid);
-    let zoneDevices = myDevices.filter(device => device.zone === zoneid);
+    homey.app.log('Now retrieve the zones for this homey');
+    let myZones = Object.values(await this.homeyAPI.zones.getZones());
+    homey.app.log('Now find all relevant zones, nested below the selected ['+includeChild+']');
+    const allRelevantZones = collectZones(myZones, zoneid, includeChild);
+    homey.app.log('Filter devices for zones='+JSON.stringify(allRelevantZones));
+    //let zoneDevices = myDevices.filter(device => device.zone === zoneid);
+    let zoneDevices = myDevices.filter(device => allRelevantZones.includes(device.zone));
+    homey.app.log('Now exclude from the current list of devices all the ones that are excluded from climate: '+zoneDevices.length)
     zoneDevices = zoneDevices.filter(device => device.settings.climate_exclude === false);
     homey.app.log('Homey Zone device count: '+zoneDevices.length);
     homey.app.log('Now filter my devices on climate capabilities');
@@ -42,7 +62,6 @@ module.exports = {
         name: device.name, 
         capabilities: Object.values(device.capabilitiesObj),
     }));
-    //homey.app.log(JSON.stringify(filteredDevices));
     homey.app.log('Now create my return object');
     let returnObj = {};
     filterCapabilities.forEach(capability => { 
